e49056b364303abc6ffdb855c839a1419353fd71
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-10 11:43:01 -0300

    Add files via upload

---
+++ lists_as_stacks_and_queues_exercise/balanced_parentheses_revision.py
@@ -0,0 +1,28 @@
+sequence = input()
+opening_brackets = []
+
+pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+}
+
+balanced = True
+
+for ch in sequence:
+    if ch in '([{':
+        opening_brackets.append(ch)
+    elif not opening_brackets:
+        balanced = False
+    else:
+        last_opening_bracket = opening_brackets.pop()
+        if pairs[last_opening_bracket] != ch:
+            balanced = False
+
+    if not balanced:
+        break
+
+if not balanced or opening_brackets:
+    print('NO')
+else:
+    print('YES')
