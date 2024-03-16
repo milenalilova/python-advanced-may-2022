69c453cc3d81453bbe42ea6047d500de71742263
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-16 10:21:28 -0300

    Add files via upload

--- multidimensional_lists_lab/even_matrix_revision.py
+++ multidimensional_lists_lab/even_matrix_revision.py
@@ -1,6 +1,24 @@
+# rows = int(input())
+#
+# matrix = [[int(j) for j in input().split(', ')] for i in range(rows)]
+# even_matrix = [[x for x in row if x % 2 == 0] for row in matrix]
+#
+# print(even_matrix)
+
+
+# Better version
+def is_even(number):
+    return number % 2 == 0
+
+
+def get_even(matrix):
+    return [x for x in matrix if is_even(x)]
+
+
 rows = int(input())
 
 matrix = [[int(j) for j in input().split(', ')] for i in range(rows)]
-even_matrix = [[x for x in row if x % 2 == 0] for row in matrix]
 
-print(even_matrix)
+print(
+    [get_even(row) for row in matrix]
+)
