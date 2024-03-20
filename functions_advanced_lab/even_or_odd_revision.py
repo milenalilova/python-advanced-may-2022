58fa7fdc1f6b47ac9824ad5b7fb25c98457a82f3
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-20 11:57:15 -0300

    Delete functions_advanced_lab/even_or_odd_revision.py

--- functions_advanced_lab/even_or_odd_revision.py
+++
@@ -1,9 +0,0 @@
-def even_odd(*args):
-    if args[-1] == 'even':
-        return [int(x) for x in args[:-1] if x % 2 == 0]
-    elif args[-1] == 'odd':
-        return [int(x) for x in args[:-1] if x % 2 != 0]
-
-
-print(even_odd(1, 2, 3, 4, 5, 6, "even"))
-print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
