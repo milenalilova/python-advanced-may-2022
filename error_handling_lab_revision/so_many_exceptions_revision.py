362f4f40236463416d0a4fef989eb390519b895f
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-21 15:44:14 -0300

    Add files via upload

---
+++ error_handling_lab_revision/so_many_exceptions_revision.py
@@ -0,0 +1,27 @@
+# Code with exceptions
+
+# numbers_list = input().split(", ")
+# result = 0
+#
+# for i in range(numbers_list):
+#     number = numbers_list[i + 1]
+#     if number < 5:
+#         result *= number
+#     elif number > 5 and number > 10:
+#         result /= number
+#
+# print(result)
+
+
+# Fixed code
+numbers_list = [int(x) for x in input().split(", ")]
+result = 1
+
+for i in range(len(numbers_list)):
+    number = numbers_list[i]
+    if number <= 5:
+        result *= number
+    elif number <= 10:
+        result /= number
+
+print(int(result))
