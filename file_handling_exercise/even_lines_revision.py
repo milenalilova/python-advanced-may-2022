1e27e29237ec11ba14efe0767473763ebe0a692b
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-24 11:49:47 -0300

    Add files via upload

---
+++ file_handling_exercise/even_lines_revision.py
@@ -0,0 +1,10 @@
+symbols = ["-", ",", ".", "!", "?"]
+
+with open('./text.txt', 'r') as file:
+    for idx, line in enumerate(file):
+        if idx % 2 == 0:
+            result = ' '.join(line.strip().split()[::-1])
+            for symbol in symbols:
+                result = result.replace(symbol, '@')
+            print(result)
+
