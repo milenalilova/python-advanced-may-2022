60a58e1e22d70e11773f245fd29f5e5338e9db0b
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-25 17:59:44 -0300

    Add files via upload

--- modules_lab_revision/calculate_logarithm_revision.py
+++ modules_lab_revision/calculate_logarithm_revision.py
@@ -8,3 +8,18 @@ if base == 'natural':
 
 else:
     print(f"{log(number, int(base)):.2f}")
+
+
+# Variant 2
+# from math import log, e
+# 
+# # Log(b)X = Y => b^y = x
+#
+# # Log(2)1024 = 10 => 2^10 = 1024
+# # Log(10)10 = 1 => 10^1 = 10
+# # Log(e)10 = 2.30 => e^2.30 = 10
+#
+#
+# print(log(10, 10))
+# print(log(10, e))
+# print(log(10))
