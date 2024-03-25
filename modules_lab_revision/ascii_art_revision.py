4cf938f331a06c8714c931baa761aa70ec73ef94
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-25 18:08:59 -0300

    Add files via upload

--- modules_lab_revision/ascii_art_revision.py
+++ modules_lab_revision/ascii_art_revision.py
@@ -1,4 +1,14 @@
 from pyfiglet import figlet_format
 
-line = input()
-print(figlet_format(line))
+
+def print_art(line):
+    ascii_art = figlet_format(line)
+    print(ascii_art)
+
+
+current_line = input()
+print_art(current_line)
+
+# Variant 2 (simple)
+# line = input()
+# print(figlet_format(line))
